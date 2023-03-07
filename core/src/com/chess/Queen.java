bec955ad7d9129bfb24a28b40c0ca7218dd8cf81
Nathan M Silva <nms204@scarletmail.rutgers.edu>
2023-03-06 22:13:12 -0500

    queen movement
    added the queen move set

--- core/src/com/chess/Queen.java
+++ core/src/com/chess/Queen.java
@@ -1,8 +1,104 @@
 package com.chess;
 
+import java.awt.*;
+
 public class Queen extends Piece{
 
     public Queen(int player) {
         super(player,5);
+    }
+
+    @Override
+    public boolean isValidMove(int row, int col, Piece piece, Board board) {
+        Piece[][] gameBoard = board.getGameBoard();
+        Point selectedTile = board.getSelectedTile();
+
+        if(piece.getPlayerID()==2){
+            if((row == selectedTile.x || col == selectedTile.y) && (gameBoard[row][col] == null || gameBoard[row][col].getPlayerID() == 1)) {
+                if(row < selectedTile.x) row++;
+                else if(row > selectedTile.x) row--;
+                else if(col < selectedTile.y) col++;
+                else col--;
+
+                while(row != selectedTile.x){
+                    if(gameBoard[row][col] != null) return false;
+
+                    if(row < selectedTile.x) row++;
+                    else row--;
+
+                }
+
+                while(col != selectedTile.y){
+                    if(gameBoard[row][col] != null) return false;
+
+                    else if(col < selectedTile.y) col++;
+                    else col--;
+                }
+                return true;
+            }
+            else if(Math.abs(row-selectedTile.x)==Math.abs(col-selectedTile.y)
+                    && (gameBoard[row][col] == null || gameBoard[row][col].getPlayerID() == 1)) {
+                if(row < selectedTile.x) row++;
+                else row--;
+
+                if(col < selectedTile.y) col++;
+                else col--;
+
+                while(row != selectedTile.x){
+                    if(gameBoard[row][col] != null) return false;
+
+                    if(row < selectedTile.x) row++;
+                    else row--;
+
+                    if(col < selectedTile.y) col++;
+                    else col--;
+                }
+                return true;
+            }
+        }
+        else{
+            if((row == selectedTile.x || col == selectedTile.y) && (gameBoard[row][col] == null || gameBoard[row][col].getPlayerID() == 2)) {
+                if(row < selectedTile.x) row++;
+                else if(row > selectedTile.x) row--;
+                else if(col < selectedTile.y) col++;
+                else col--;
+
+                while(row != selectedTile.x){
+                    if(gameBoard[row][col] != null) return false;
+
+                    if(row < selectedTile.x) row++;
+                    else row--;
+
+                }
+
+                while(col != selectedTile.y){
+                    if(gameBoard[row][col] != null) return false;
+
+                    else if(col < selectedTile.y) col++;
+                    else col--;
+                }
+                return true;
+            }
+            else if(Math.abs(row-selectedTile.x)==Math.abs(col-selectedTile.y)
+                    && (gameBoard[row][col] == null || gameBoard[row][col].getPlayerID() == 2)) {
+                if(row < selectedTile.x) row++;
+                else row--;
+
+                if(col < selectedTile.y) col++;
+                else col--;
+
+                while(row != selectedTile.x){
+                    if(gameBoard[row][col] != null) return false;
+
+                    if(row < selectedTile.x) row++;
+                    else row--;
+
+                    if(col < selectedTile.y) col++;
+                    else col--;
+                }
+                return true;
+            }
+        }
+        return false;
     }
 }
