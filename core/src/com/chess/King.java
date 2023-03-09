3ee07d20c481aa4ee9196909400f5dc07004ca04
Nathan M Silva <nms204@scarletmail.rutgers.edu>
2023-03-08 19:37:18 -0500

    basic king movement
    added basic king move set, castling and check are yet to be added

--- core/src/com/chess/King.java
+++ core/src/com/chess/King.java
@@ -1,8 +1,28 @@
 package com.chess;
 
+import java.awt.*;
+
 public class King extends Piece{
 
     public King(int player) {
         super(player,6);
+    }
+
+    @Override
+    public boolean isValidMove(int row, int col, Piece piece, Board board){
+        Piece[][] gameBoard = board.getGameBoard();
+        Point selectedTile = board.getSelectedTile();
+
+        if(piece.getPlayerID()==2){
+            if((Math.abs(row-selectedTile.x) <= 1 && Math.abs(col-selectedTile.y) <= 1)
+                    && (gameBoard[row][col] == null || gameBoard[row][col].getPlayerID() == 1))
+                return true;
+        }
+        else{
+            if((Math.abs(row-selectedTile.x) <= 1 && Math.abs(col-selectedTile.y) <= 1)
+                    && (gameBoard[row][col] == null || gameBoard[row][col].getPlayerID() == 2))
+                return true;
+        }
+        return false;
     }
 }
